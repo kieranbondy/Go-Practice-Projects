2f1b3cffb7d322a1ee928dd2ec1e7e801ba28b42
Kieran B <k.bondman7@gmail.com>
2024-02-27 17:05:40 -0500

    Cards project completed

--- cards/deck.go
+++ cards/deck.go
@@ -1,13 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+)
 
 // Create a new type of deck
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardNumbers := []string{"Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"}
+	for _, suit := range cardSuits {
+		for _, val := range cardNumbers {
+			cards = append(cards, val+" of "+suit)
+		}
+	}
+	return cards
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(fileName string) deck {
+	bs, err := os.ReadFile(fileName)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		np := rand.Intn(len(d) - 1)
+		d[i], d[np] = d[np], d[i]
 	}
 }
--- cards/main.go
+++ cards/main.go
@@ -1,12 +1,8 @@
 package main
 
 func main() {
-	cards := deck{newCard(), newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeckFromFile("myCards")
+	cards.shuffle()
 	cards.print()
 
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
